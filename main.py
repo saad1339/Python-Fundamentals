9553196da15d98171a105e6cafb0ade3567a3aec
Muhammad Saad Qayyum <ms1339616@gmail.com>
2026-07-19 16:34:44 +0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-
-print ("********************************");
-print ("*  Superior University         *");
-print ("*  Muhammad Saad Qayyum        *");
-print ("*  Roll No: SU92-BSITM-F24-213 *");
-print ("*  class    BSITM-F24          *");
-print ("********************************");
-
