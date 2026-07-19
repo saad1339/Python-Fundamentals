7921a30bc555708372ccb0d0c48591298580c205
saad1339 <ms1339616@gmail.com>
2026-07-19 15:44:38 +0500

    Output commands

---
+++ Print Practice.py
@@ -0,0 +1,8 @@
+
+print ("********************************");
+print ("*  Superior University         *");
+print ("*  Muhammad Saad Qayyum        *");
+print ("*  Roll No: SU92-BSITM-F24-213 *");
+print ("*  class    BSITM-F24          *");
+print ("********************************");
+
